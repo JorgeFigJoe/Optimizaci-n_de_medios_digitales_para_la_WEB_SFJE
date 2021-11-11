c9576ce8f816b8ecb271bb2017445f6459eb33d0
Jorge Figueroa <50466247+JorgeFigJoe@users.noreply.github.com>
2021-11-11 00:02:02 -0600

    modify db

--- registro.php
+++ registro.php
@@ -1,6 +1,7 @@
 <?php
+
 //http://localhost/plb/Proyecto_Integradora_SFJE/index.php
-$conexion=mysqli_connect('us-cdbr-east-04.cleardb.com','b4ff24aaa4bb61','a63d9274','heroku_4025336ab2aaf31');
+$conexion= new mysqli('us-cdbr-east-04.cleardb.com','b4ff24aaa4bb61','a63d9274','heroku_4025336ab2aaf31');
 
 	$name=$_POST['name'];
 	$comment=$_POST['comment'];
