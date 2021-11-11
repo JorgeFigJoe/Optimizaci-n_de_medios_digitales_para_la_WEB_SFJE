7736a1a0a42e517a49ee555dbd5f9c67fe4a63d1
Jorge Figueroa <50466247+JorgeFigJoe@users.noreply.github.com>
2021-11-10 23:59:36 -0600

    add database

--- registro.php
+++ registro.php
@@ -5,6 +5,18 @@ $conexion=mysqli_connect('us-cdbr-east-04.cleardb.com','b4ff24aaa4bb61','a63d927
 	$name=$_POST['name'];
 	$comment=$_POST['comment'];
 
+// Verificamos la conexión con el servidor MySQL
+if ($conexion->connect_error) {
+    die("la conexión ha fallado: " . $conexion->connect_error);
+}
+
+// sql Crea la tabla usando Lenguaje PHP
+$sql = "CREATE TABLE IF NOT EXISTS comentarios (
+id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+name VARCHAR(150) NOT NULL,
+comment VARCHAR(150) NOT NULL
+)";
+
 	$sql="INSERT into comentarios (name,comment)
 			values ('$name','$comment')";
 	echo mysqli_query($conexion,$sql);
