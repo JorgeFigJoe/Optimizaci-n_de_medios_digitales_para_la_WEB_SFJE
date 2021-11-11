71691a4852ad131e1349b5a4c8a0812e92ddbd0b
Jorge Figueroa <50466247+JorgeFigJoe@users.noreply.github.com>
2021-11-11 00:45:24 -0600

    fix

--- allComents.php
+++ allComents.php
@@ -1,6 +1,7 @@
 <?php
-    $conexion=mysqli_connect('us-cdbr-east-04.cleardb.com','b4ff24aaa4bb61','a63d9274','heroku_4025336ab2aaf31');
+    //$conexion=mysqli_connect('us-cdbr-east-04.cleardb.com','b4ff24aaa4bb61','a63d9274','heroku_4025336ab2aaf31');
    //$conexion = mysqli_connect('localhost','root','','testimonios');
+   $conexion= new mysqli('us-cdbr-east-04.cleardb.com','b4ff24aaa4bb61','a63d9274','heroku_4025336ab2aaf31');
     $myArray = array();
 
     $sql = "SELECT * FROM comentarios";
