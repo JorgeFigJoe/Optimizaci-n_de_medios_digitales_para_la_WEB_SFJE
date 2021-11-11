c8cdbbad2442a7c62e7d40afcaee5bcc4e3f584a
Jorge Figueroa <50466247+JorgeFigJoe@users.noreply.github.com>
2021-11-11 00:21:21 -0600

    [BugFix] fix show table

--- allComents.php
+++ allComents.php
@@ -1,5 +1,6 @@
 <?php
     $conexion=mysqli_connect('us-cdbr-east-04.cleardb.com','b4ff24aaa4bb61','a63d9274','heroku_4025336ab2aaf31');
+   //$conexion = mysqli_connect('localhost','root','','testimonios');
     $myArray = array();
 
     $sql = "SELECT * FROM comentarios";
--- registro.php
+++ registro.php
@@ -2,7 +2,7 @@
 
 //http://localhost/plb/Proyecto_Integradora_SFJE/index.php
 $conexion= new mysqli('us-cdbr-east-04.cleardb.com','b4ff24aaa4bb61','a63d9274','heroku_4025336ab2aaf31');
-
+//$conexion = mysqli_connect('localhost','root','','testimonios');
 	$name=$_POST['name'];
 	$comment=$_POST['comment'];
 
